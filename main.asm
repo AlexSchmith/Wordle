e66a48d338c065bdbff0f4efdfc697f5e5b06f37
Nick Cottrell <ncottrellweb@gmail.com>
2022-03-29 18:29:54 -0400

    Added looper to display all available words in wordlist

--- main.asm
+++ main.asm
@@ -3,12 +3,22 @@
 .stack 4096
 ExitProcess PROTO, dwExitCode:DWORD
 
+INCLUDE Irvine32.inc
 .data
-wordlist BYTE 100 (?)
+wordlist BYTE "which",0,"there",0,"their",0,"about",0,"would",0,"these",0,"other",0,"words",0,"could",0,"write",0,"first",0,"water",0,"after",0,"where",0,"right",0,"think",0,"three", 0
 
 .code
 main PROC PUBLIC
-
+    mov ecx, 17
+    mov esi, 0
+    mov edx, OFFSET wordlist
+    WordlistLoop:
+        ;// mov edx, OFFSET wordlist
+        call WriteString
+        call Crlf
+        add edx, 6 * SIZEOF BYTE
+        inc esi
+        loop WordlistLoop
     INVOKE ExitProcess, 0
 main ENDP
 END main
