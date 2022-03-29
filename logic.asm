6c9c0c3ed02375e9a9dbfcd35bd82ef722673d57
Nick Cottrell <ncottrellweb@gmail.com>
2022-03-29 17:44:48 -0400

    Added function prototypes to logic

--- logic.asm
+++ logic.asm
@@ -1,1 +1,20 @@
 INCLUDE logic.inc
+
+.code
+;// outputs pointer to random word in wordlist to act as word chooser
+SelectRandomWord PROC
+
+SelectRandomWord ENDP
+;// gets the number of different characters
+CheckDifference PROC, word1: BYTE, word2: BYTE
+
+CheckDifference ENDP
+;// check if letter is in word
+CharInWord PROC, char: BYTE, WordCheck: BYTE
+
+CharInWord ENDP
+;// check if letter in same spot
+CharInSamePos PROC, word1: BYTE, word2: BYTE, pos: BYTE
+
+CharInSamePos ENDP
+END
