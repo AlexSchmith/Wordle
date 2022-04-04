99ff4ba3343396e1390a21148be1738d62e4147e
Nick Cottrell <ncottrellweb@gmail.com>
2022-04-04 14:19:48 -0400

    Removed checkdifference since strcmp exists

--- logic.asm
+++ logic.asm
@@ -10,10 +10,6 @@ SelectRandomWord PROC USES eax
     add edx, 6 * SIZEOF BYTE
     ret
 SelectRandomWord ENDP
-;// gets the number of different characters
-CheckDifference PROC, word1: BYTE, word2: BYTE
-    ret
-CheckDifference ENDP
 ;// check if letter is in word
 CharInWord PROC, char: BYTE, WordCheck: BYTE
     ret
