40acc8a9fa8902c8fa16caaefdc418bedbad6a4c
Alex Schmith <aschmith2019@my.fit.edu>
2022-04-05 08:53:30 -0500

    Fixed graphics issue with reading strings

--- main.asm
+++ main.asm
@@ -12,7 +12,7 @@ MAX = 5
 
 wordlist BYTE "which",0,"there",0,"their",0,"about",0,"would",0,"these",0,"other",0,"words",0,"could",0,"write",0,"first",0,"water",0,"after",0,"where",0,"right",0,"think",0,"three", 0
 testword BYTE "light"
-input BYTE MAX + 1 DUP (?)
+input BYTE 6 DUP (?)
 
 
 .code
@@ -21,10 +21,13 @@ main PROC PUBLIC
     call SetDisplay
     call Randomize
     
-    mov ecx, MAX
+    mov ecx, 5
     mov edx, OFFSET input
     call ReadString
 
+    mov DH, 15
+    mov DL, 50
+    call GotoXY
 
     push OFFSET wordlist
     push OFFSET input
