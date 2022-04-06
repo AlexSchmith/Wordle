372efe4f68919d16b0610412014048db1a8e5091
Nick Cottrell <ncottrellweb@gmail.com>
2022-04-06 15:57:08 -0400

    Added more comments to logic

--- logic.asm
+++ logic.asm
@@ -18,21 +18,27 @@ SelectRandomWord PROC USES eax ebx
 SelectRandomWord ENDP
 ;// check if letter is in word
 CharInWord PROC USES eax ecx esi, Char: BYTE, WordCheck : DWORD
+    ;// set limit of loop to 5. The max number of characters to a word
     mov ecx, 5
     mov esi, WordCheck
     CharInWordLoop:
+        ;// loop through each letter
         mov ah, [esi]
         cmp ah, Char
+        ;// if chars are equal, go to end. leave cmp value to true
         je CharInWordEnd
         inc esi
         loop CharInWordLoop
+    ;// if never equal, last cmp statement gives false
     CharInWordEnd:
     ret
 CharInWord ENDP
 ;// check if letter in same spot
 CharInSamePos PROC USES eax, Char1: BYTE, Char2 : BYTE
+    ;// simply compare two characters
     mov ah, Char1
     cmp ah, Char2
+    ;// leave values from cmp to return true or false
     ret
 CharInSamePos ENDP
 END
