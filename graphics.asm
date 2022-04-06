7b87c55dfe02ba5be06a74dc49ec6262758dd799
Nick Cottrell <ncottrellweb@gmail.com>
2022-04-06 16:11:48 -0400

    Removed excessive whitespace use in graphics

--- graphics.asm
+++ graphics.asm
@@ -6,7 +6,6 @@ INCLUDE graphics.inc
 ExitProcess PROTO, dwExitCode: DWORD
 
 .data
-
 winners BYTE "$$\     $$\                                                           ", 0
 winner1 BYTE "\$$\   $$  |                                                          ", 0
 winner2 BYTE " \$$\ $$  /$$$$$$\  $$\   $$\       $$\  $$\  $$\  $$$$$$\  $$$$$$$\  ", 0
@@ -15,9 +14,6 @@ winner4 BYTE "   \$$  / $$ /  $$ |$$ |  $$ |      $$ | $$ | $$ |$$ /  $$ |$$ |  
 winner5 BYTE "    $$ |  $$ |  $$ |$$ |  $$ |      $$ | $$ | $$ |$$ |  $$ |$$ |  $$ |", 0
 winner6 BYTE "    $$ |  \$$$$$$  |\$$$$$$  |      \$$$$$\$$$$  |\$$$$$$  |$$ |  $$ |", 0
 winner7 BYTE "    \__|   \______/  \______/        \_____\____/  \______/ \__|  \__|", 0
-
-
-
 
 wordy 	BYTE " ___       __   ________  ________  ________  ___       _______      ",0
 wordy1	BYTE "|\  \     |\  \|\   __  \|\   __  \|\   ___ \|\  \     |\  ___ \     ",0
@@ -35,18 +31,13 @@ nomaidens4 BYTE "    |  |    |  `--'  | |  `--'  |   |  `----.|  `--'  | .----) 
 nomaidens5 BYTE "    |__|     \______/   \______/    |_______| \______/  |_______/       |__|     ", 0
 
 ending 	BYTE "Sorry. The Word of the Day was: ",0 
-
-
 empty BYTE "MEME"  
-
 start_box_h BYTE 15
 
 .code
-
 ; Function that checks each character in the word and displays them
 ; Uses esi for the wod and edi for the word
 CheckWord PROC uses eax ecx esi edi, current_word: DWORD, wod: DWORD
-	
 	;use al for each individual char
 	mov ecx, 5
 	mov esi, current_word
@@ -56,11 +47,9 @@ CheckWord PROC uses eax ecx esi edi, current_word: DWORD, wod: DWORD
 		push [edi]
 		push [esi]
 		call CharInSamePos
-	
 		jne NotCorrect
 		mov ebx, 2
 		jmp Display
-
 		NotCorrect:
 		push wod
 		push [esi]
@@ -68,10 +57,8 @@ CheckWord PROC uses eax ecx esi edi, current_word: DWORD, wod: DWORD
 		jne Wrong
 		mov ebx, 1
 		jmp Display
-		
 		Wrong:
 		mov ebx, 0
-	
 		Display:
 		push eax
 		push ebx
@@ -79,7 +66,6 @@ CheckWord PROC uses eax ecx esi edi, current_word: DWORD, wod: DWORD
 		inc esi
 		inc edi
 		loop WordLoop
-
 	ret 
 CheckWord ENDP
 
@@ -91,7 +77,6 @@ DisplayChar PROC uses ebx eax, color_bg: BYTE, char: BYTE
 	cmp ebx, 1
 	je inword
 	push eax
-	
 	Correct:
 		mov eax, white + (lightGreen * 16)
 		call SetTextColor
@@ -112,22 +97,18 @@ DisplayChar PROC uses ebx eax, color_bg: BYTE, char: BYTE
 		mov eax, black + (white * 16)
 		call SetTextColor
 		ret 
-
 DisplayChar ENDP
 
 ; Setup display to set background color and move cursor to the right position
 SetDisplay PROC uses eax edx
-	
 	mov eax, gray + (lightGray * 16)
 	call SetTextColor
-
 	call Clrscr
 	mov DH, 5
 	mov DL, 20
 	call GotoXY
-
 	call Wordle
-	
+
 	mov DH, 15
     mov DL, 50
     call    GotoXY
@@ -138,117 +119,86 @@ SetDisplay PROC uses eax edx
 	mov esi, 0
 	mov ecx, 6
 	sub DH, 1
-	
 	BoxLoop:
 		mov eax, gray + (gray * 16)
 		call SetTextColor
 		push edx
-
 		mov edx, OFFSET empty
 		call WriteString
 		mov edx, 0
-
 		pop edx
 		mov eax, white + (white * 16)
 		call SetTextColor
-
 		add DH, 1
 		mov DL, 50
 		call GotoXY
-
 		inc esi
 		loop BoxLoop
-	
 	mov DH, 15
 	mov DL, 50
 	call GotoXY
 
 	mov eax, gray + (gray * 16)
 	call SetTextColor
-
 	ret
-
 SetDisplay ENDP
-
-
 
 Winner PROC
 	mov eax, white + (white * 16)
 	call SetTextColor
 	call ClrScr
-
 	mov eax, green + (white * 16)
 	call SetTextColor
-
 	mov DH, 5
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET winners
 	call WriteString
-
 	mov DH, 6
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET winner1
 	call WriteString
-
 	mov DH, 7
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET winner2
 	call WriteString
-	
 	mov DH, 8
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET winner3
 	call WriteString
-	
 	mov DH, 9
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET winner4
 	call WriteString
-
 	mov DH, 10
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET winner5
 	call WriteString
-
 	mov DH, 11
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET winner6
 	call WriteString
-
-
 	mov DH, 12
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET winner7
 	call WriteString
 
 	mov eax, 10000
 	call Delay
-
 	ret
-	
 Winner ENDP
 
 Loser PROC, wod: DWORD
 	mov eax, white + (white * 16)
 	call SetTextColor
 	call ClrScr
-
 	mov eax, lightRed + (white * 16)
 	call SetTextColor
 
@@ -258,125 +208,83 @@ Loser PROC, wod: DWORD
 
 	mov edx, OFFSET nomaidens
 	call WriteString
-
 	mov DH, 6
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET nomaidens1
 	call WriteString
-
 	mov DH, 7
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET nomaidens2
 	call WriteString
-	
 	mov DH, 8
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET nomaidens3
 	call WriteString
-	
 	mov DH, 9
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET nomaidens4
 	call WriteString
-
 	mov DH, 10
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET nomaidens5
 	call WriteString
-
 	mov DH, 20
 	mov DL, 40
 	call GotoXY
-
 	mov edx, OFFSET ending
 	call WriteString
 	mov edx, wod
 	call WriteString
 
-
 	mov eax, 10000
 	call Delay
-
 	ret
-
 Loser ENDP
-
 
 Wordle PROC
 	mov eax, lightMagenta + (lightGray* 16)
 	call SetTextColor
 
-	
 	mov edx, OFFSET wordy
 	call WriteString
-	
-
 	mov DH, 6
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET wordy1
 	call WriteString
-	
-
 	mov DH, 7
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET wordy2
 	call WriteString
-	
-
 	mov DH, 8
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET wordy3
 	call WriteString
-	
-
 	mov DH, 9
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET wordy4
 	call WriteString
-
-
 	mov DH, 10
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET wordy5
 	call WriteString
-
-
 	mov DH, 11
 	mov DL, 20
 	call GotoXY
-
 	mov edx, OFFSET wordy6
 	call WriteString
-	
-
 	mov DH, 12
 	mov DL, 20
 	call GotoXY
-
 	ret
-
 Wordle ENDP
-
-
-
 END
