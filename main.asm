a8ea49f30bfc17ff9b5282c2ae79d104b02aee53
Alex Schmith <aschmith2019@my.fit.edu>
2022-04-20 13:30:31 -0500

    Added option to try again

--- main.asm
+++ main.asm
@@ -14,15 +14,18 @@ bufferWord BYTE 7 DUP (?), 0
 tries BYTE 0
 square_length BYTE 50
 square_height BYTE 15
+try_again BYTE "Press y to try again",0
 
 .code
 main PROC PUBLIC
     ; start by getting random word
     call Randomize
-    call SelectRandomWord
-    mov wod, edx
-    ; setting display
-    call SetDisplay
+    GameOn:
+        call SelectRandomWord
+        mov wod, edx
+        mov tries, 0
+        ; setting display
+        call SetDisplay
     LoopRows:
         mov dh, square_height
         add dh, tries
@@ -78,6 +81,18 @@ main PROC PUBLIC
         ; looks like theyve won. Time to show it
         call Winner
     Stop:
+        
+        mov dl, 25
+        mov dh, 25
+        call GotoXY
+        mov eax, fontColor
+        call SetTextColor
+        mov edx, OFFSET try_again
+        call WriteString
+        call ReadChar
+        cmp al, 'y'
+        je GameOn
+       
         INVOKE ExitProcess, 0
 main ENDP
 END main
